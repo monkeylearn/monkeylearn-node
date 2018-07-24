59c2a564efc1ca42b3892a1b1c40ddb290ef3b15
brusteca <bruno@monkeylearn.com>
2018-07-24 18:07:44 -0300

    added retry if throttled

--- index.js
+++ index.js
@@ -2,24 +2,23 @@
 
 module.exports = MonkeyLearn;
 
+const defaults = require('lodash/defaults');
+
 const Classifiers = require('./lib/classifiers');
 
-function MonkeyLearn(api_key, base_url) {
+function MonkeyLearn(api_key, settings) {
   if (api_key == undefined) {
     throw new Error('A MonkeyLearn API key is required to use the API. Find yours at https://app.monkeylearn.com/main/my-account/tab/api-keys/');
   }
-  base_url = base_url || 'https://api.monkeylearn.com/v3/'
+  settings = settings || {};
+  defaults(settings, {
+    base_url: 'https://api.monkeylearn.com/v3/',
+    retry_if_throttled: true,
+    throttling_max_retries: 3
+  })
 
-  const options = {
-    api_key: api_key,
-    base_url: base_url
-  }
-
-  const api = {
-    api_key: api_key,
-    base_url: base_url,
-    classifiers: new Classifiers(options),
-    extractors: 0
-  }
-  return api
+  this.api_key = api_key;
+  this.settings = settings;
+  this.classifiers = new Classifiers(this);
+  this.extractors = 0;
 }
