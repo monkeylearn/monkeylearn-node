6c87be17b78ac484604d6807267c511fbe92ad77
Akshay Kumar <work.akshay.kumar@gmail.com>
2021-07-01 16:34:30 +0530

    Re-export all types via an index.d.ts file

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+import Classifiers from "./classifiers";
+import Extractors from "./extractors";
+import Models from "./models";
+import MonkeyLearnError from "./monkeylearn-error";
+import MonkeyLearnResponse from "./response";
+import Workflows from "./workflows";
+
+export {
+  Classifiers,
+  Extractors,
+  Models,
+  MonkeyLearnError,
+  MonkeyLearnResponse,
+  Workflows,
+};
+export * from "./request";
+export * from "./utils";
